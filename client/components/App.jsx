9f575b954b9e7e0731e5e7c375a17f58d2c32d0f
daniel-reason <dreason@live.co.uk>
2018-03-07 15:14:42 +1300

    create base site from boilerplate

---
+++ client/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Home from './Home'
+
+function App (props) {
+  return (
+    <div className='app-container'>
+      <Home />
+    </div>
+  )
+}
+
+export default App
