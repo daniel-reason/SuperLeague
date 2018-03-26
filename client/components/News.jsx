04bc15ce711513d47ff1d48edf5452d948865bec
daniel-reason <dreason@live.co.uk>
2018-03-26 13:18:38 +1300

    add news page

---
+++ client/components/News.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const News = ({props}) => (
+  <div className="container">
+
+    <div className="newsContent">
+      <div className="eight columns" id="homeBoxes">
+        <div className="newsContent">
+          Latest news stuff
+        </div>
+      </div>
+    </div>
+  </div>
+)
+
+export default News
